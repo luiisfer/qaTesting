9f4299b21583fbd55d1038b53f1d2d5c27eae649
LuisL <luiisfer_1992@hotmail.com>
2021-05-01 01:24:26 -0600

    Segundo Commit1

--- src/test/resources/JenkinsFile.groovy
+++ src/test/resources/JenkinsFile.groovy
@@ -1,23 +1,9 @@
-def checkout = { String subFolder ->
-    checkout([
-            $class      : "GitSCM",
-            branches    : [[name: "*/master"]],
-            extensions  : [[ $class: "RelativeTargetDirectory",
-                             relativeTargetDir: subFolder
-                           ]],
-            userRemoteConfigs: [[
-                                        url : 'git@github.com:josewolff/core-code-QA-program-2021.git',
-                                        credentialsId: "jenkins"
-                                ]]
-    ])
-}
-
 def runner = { commandToExecute -> isUnix() ? sh(commandToExecute) : bat(commandToExecute) }
 
 
 unitTest:{
     stage("Unit Testing"){
-        node('master'){
+        node('MyNewNode'){
             checkout("tests")
             runner 'cd tests && mvn test -DincludeGroup=unitTesting'
         }
@@ -27,7 +13,7 @@ IntegrationTest:{
     stage("Integration Testing"){
         node('MyNewNode'){
             checkout("tests")
-            runner 'cd tests && mvn test -DincludeGroup=apiTesting -DapiHost=${apiHost}'
+            runner 'cd tests && mvn test -DincludeGroup=unitTesting'
         }
     }
 }
